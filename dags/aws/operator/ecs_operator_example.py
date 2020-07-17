f9d54f52377e7b8b7dba3d512975b8504f5f784a
matsudan <daaamatsun@gmail.com>
2020-07-18 05:11:28 +0900

    add ecsoperator example

---
+++ dags/aws/operator/ecs_operator_example.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.utils.dates import days_ago
+from airflow.contrib.operators.ecs_operator import ECSOperator
+
+config = Variable.get("ecs_config", deserialize_json=True)
+
+default_args = {
+    "owner": "example",
+    "start_date": days_ago(2),
+    "retries": 1,
+    "retry_delay": timedelta(minutes=60),
+    "retry_exponential_backoff": True,
+}
+
+with DAG(
+        dag_id="ecs",
+        default_args=default_args,
+        schedule_interval="@once",
+) as dag:
+    ecs_task = ECSOperator(
+        task_id="ecs_task",
+        task_definition=config["task_definition"],
+        cluster=config["cluster"],
+        overrides=config["overrides"],
+        launch_type=config["launch_type"],
+        network_configuration=config["network_configuration"]
+    )
