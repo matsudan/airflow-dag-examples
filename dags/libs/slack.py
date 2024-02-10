195cca550fb2ff138273de6d4c1dd0db29d07219
matsudan <daaamatsun@gmail.com>
2024-02-11 03:51:26 +0900

    Fix sla miss message

--- dags/libs/slack.py
+++ dags/libs/slack.py
@@ -3,20 +3,24 @@ from airflow import DAG
 from airflow.models import Variable
 
 
-def _post_to_slack(color: str, text: str) -> None:
+def _post_to_slack(color: str, text: str, attachment_text: str) -> None:
     config = Variable.get("slack", deserialize_json=True)
 
     requests.post(
         config["webhook_url"],
         json={
+            "blocks": [{"type": "section", "text": {"type": "mrkdwn", "text": text}}],
             "attachments": [
                 {
                     "color": color,
                     "blocks": [
-                        {"type": "section", "text": {"type": "mrkdwn", "text": text}}
+                        {
+                            "type": "section",
+                            "text": {"type": "mrkdwn", "text": attachment_text},
+                        }
                     ],
                 }
-            ]
+            ],
         },
     )
 
@@ -25,10 +29,13 @@ def post_sla_miss_to_slack(
     dag: DAG, task_list: str, blocking_task_list: str, slas: list, blocking_tis: list
 ) -> None:
 
-    text = (
-        f"@channel SLA was missed on {dag.dag_id}.\n"
+    att_text = (
         f"Task ID: {slas[0].task_id}\n"
         f"TaskInstance ID: {blocking_tis[0]}\n"
     )
 
-    _post_to_slack("#ffa500", text)
+    _post_to_slack(
+        "#ffa500",
+        f"@channel SLA was missed on {dag.dag_id}.\n",
+        att_text,
+    )
