6c078cb00ca7f30b7f1d17bbb2c93e4c2f2a46c2
matsudan <daaamatsun@gmail.com>
2024-02-11 02:45:50 +0900

    Add slack notification function

---
+++ dags/libs/slack.py
@@ -0,0 +1,34 @@
+import requests
+from airflow import DAG
+from airflow.models import Variable
+
+
+def _post_to_slack(color: str, text: str) -> None:
+    config = Variable.get("slack", deserialize_json=True)
+
+    requests.post(
+        config["webhook_url"],
+        json={
+            "attachments": [
+                {
+                    "color": color,
+                    "blocks": [
+                        {"type": "section", "text": {"type": "mrkdwn", "text": text}}
+                    ],
+                }
+            ]
+        },
+    )
+
+
+def post_sla_miss_to_slack(
+    dag: DAG, task_list: str, blocking_task_list: str, slas: list, blocking_tis: list
+) -> None:
+
+    text = (
+        f"@channel SLA was missed on {dag.dag_id}.\n"
+        f"Task ID: {slas[0].task_id}\n"
+        f"TaskInstance ID: {blocking_tis[0]}\n"
+    )
+
+    _post_to_slack("#ffa500", text)
