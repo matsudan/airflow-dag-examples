ca8a0c36e5f3a8b8300702a70a15c315ab899bad
matsudan <daaamatsun@gmail.com>
2024-02-16 23:52:31 +0900

    Fix slack lib

--- dags/libs/slack.py
+++ dags/libs/slack.py
@@ -1,41 +1,69 @@
+from dataclasses import dataclass
+from functools import wraps
+
 import requests
 from airflow import DAG
 from airflow.models import Variable
 
 
-def _post_to_slack(color: str, text: str, attachment_text: str) -> None:
-    config = Variable.get("slack", deserialize_json=True)
-
-    requests.post(
-        config["webhook_url"],
-        json={
-            "blocks": [{"type": "section", "text": {"type": "mrkdwn", "text": text}}],
-            "attachments": [
-                {
-                    "color": color,
-                    "blocks": [
-                        {
-                            "type": "section",
-                            "text": {"type": "mrkdwn", "text": attachment_text},
-                        }
-                    ],
-                }
-            ],
-        },
-    )
+@dataclass
+class SlackMessageConfig:
+    color: str
+    text: str
+    attachment_text: str
 
 
+def post_to_slack(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        c = func(*args, **kwargs)
+        config = Variable.get("slack", deserialize_json=True)
+
+        requests.post(
+            config.get("webhook_url"),
+            json={
+                "blocks": [
+                    {"type": "section", "text": {"type": "mrkdwn", "text": c.text}}
+                ],
+                "attachments": [
+                    {
+                        "color": c.color,
+                        "blocks": [
+                            {
+                                "type": "section",
+                                "text": {"type": "mrkdwn", "text": c.attachment_text},
+                            }
+                        ],
+                    }
+                ],
+            },
+        )
+
+    return wrapper
+
+
+@post_to_slack
 def post_sla_miss_to_slack(
     dag: DAG, task_list: str, blocking_task_list: str, slas: list, blocking_tis: list
-) -> None:
+) -> SlackMessageConfig:
 
-    att_text = (
-        f"Task ID: {slas[0].task_id}\n"
-        f"TaskInstance ID: {blocking_tis[0]}\n"
-    )
-
-    _post_to_slack(
+    return SlackMessageConfig(
         "#ffa500",
         f"@channel SLA was missed on {dag.dag_id}.\n",
-        att_text,
+        f"Task ID: {slas[0].task_id}\n" f"TaskInstance ID: {blocking_tis[0]}\n",
+    )
+
+
+@post_to_slack
+def post_failure_to_slack(context: dict) -> SlackMessageConfig:
+
+    return SlackMessageConfig(
+        "#ff0000",
+        f"@channel Task has failed on {context.get('task_instance').dag_id}.\n",
+        (
+            f"Task ID: {context.get('task').task_id}\n"
+            f"Execution time: {context.get('logical_date')}\n"
+            f"Reason: {context.get('reason')}\n"
+            f"Log URL: {context.get('task_instance').log_url}\n"
+        ),
     )
